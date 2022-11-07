84d5a9bf355870ad1f79c53d9eb75503dcc00991
Johnson Liu <johnson.liu@nesswell.com>
2022-11-07 14:34:22 -0500

    2022-11-07, add in the Alamofire and SwiftJSON, via the Cocoapods.

--- MyJsonTest2/MyJsonTest2/ViewController.swift
+++ MyJsonTest2/MyJsonTest2/ViewController.swift
@@ -12,6 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "Home"
+        self.title = "Home 2"
     }
 }
