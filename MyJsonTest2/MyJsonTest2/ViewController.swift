b0f38f44d411479e48a32063ff2baa0b40e1cf09
Johnson Liu <johnson.liu@nesswell.com>
2022-11-07 13:56:16 -0500

    2022-11-07 : initial save for the new project.

---
+++ MyJsonTest2/MyJsonTest2/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyJsonTest2
+//
+//  Created by Johnson Liu on 11/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Home"
+    }
+}
